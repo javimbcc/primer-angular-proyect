29e0b3e7523e9b9ce7cc945ad4b14f7fda0e5c6a
JAVIER\menaj <menajavier2002@gmail.com>
2022-10-17 14:42:39 +0200

    añadido mi primer componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MiComponentComponent } from './mi-component/mi-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mi-component/mi-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mi-component',
+  templateUrl: './mi-component.component.html',
+  styleUrls: ['./mi-component.component.css']
+})
+export class MiComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
